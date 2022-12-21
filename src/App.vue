1feb613fe9d8f09c18e7cc4c32ec77971379f3d9
lufranski <lufranski@gmail.com>
2022-12-21 11:22:03 +0100

    Comments

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,23 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- Header -->
+      <!-- Topbar con contatti -->
+      <!-- Navbar con logo e menu -->
+      <!-- Hero -->
+
+  <!-- Main -->
+      <!-- Section con cards - Business Areas -->
+      <!-- Section con bg e list items - The Company  -->
+      <!-- Section con nav menu e cards con bg img - Actions & Projects -->
+      <!-- Section con list items e bg img - Results -->
+      <!-- Form section - Get in Touch -->
+
+  <!-- Footer -->
+      <!-- Flex container con contatti + card con list items -->
+      <!-- Section con pulsante "sali su" -->
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
 </style>
